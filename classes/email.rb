30985b064c7eaf8c693f47c3f2590f8b2b4e888c
Anne Byrne <annebyrne.e@gmail.com>
2016-02-28 15:48:10 +0000

    set up email.rb for mailgun

--- classes/email.rb
+++ classes/email.rb
@@ -1,1 +1,20 @@
 require_relative "../pony"
+
+Pony.options = {
+	:via => 'smtp',
+	:via_options => {
+		:address => 'smtp.mailgun.org',
+		:port => '587',
+		:enable_starttls_auto => true,
+		:authentication => :plain,
+		:user_name => 
+		:password =>
+	}
+}
+
+message = {
+	:from => 'dailydigest247@gmail.com',
+	:to => :name <:email>,
+	:subject => 'Your Digest',
+	:body => ' '
+}
